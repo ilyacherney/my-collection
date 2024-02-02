7fb350fa42b72228612de12ee27f29e5a66d5ea3
Ilya Cherney <ilyacherney@yandex.ru>
2024-02-02 09:44:07 +0300

    init

---
+++ src/main/java/Test1.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + age;
+    }
+}
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Carol", 35));
+
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return Integer.compare(p1.age, p2.age);
+            }
+        });
+
+        for (Person p : people) {
+            System.out.println(p);
+        }
+    }
+}
