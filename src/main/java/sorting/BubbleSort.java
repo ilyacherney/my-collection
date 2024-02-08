46faa5f89aca5ab1426c8eeca0bb0672d12e8baa
Ilya Cherney <ilyacherney@yandex.ru>
2024-02-08 10:54:55 +0300

    Произвёл рефакторинг и задействовал корпаратор

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -3,35 +3,28 @@ package sorting;
 import java.util.Comparator;
 
 public class BubbleSort {
+    static boolean elementSwitched = false;
     public static int[] sort(int[] array, SortingOrder sortingOrder, Comparator comparator) {
-        boolean elementReplaced = false;
-        int buffer;
-        switch (sortingOrder) {
-            case ASCENDING:
-                for (int i = 0; i < array.length; i++) {
-                    if (i == array.length - 1) break;
-//                        if (array[i] > array[i + 1]) {
-                    if (comparator.compare(array[i], array[i + 1]) == 1) {
-                        buffer = array[i + 1];
-                        array[i + 1] = array[i];
-                        array[i] = buffer;
-                        elementReplaced = true;
-                    }
-                }
-                break;
-            case DESCENDING:
-                for (int i = 0; i < array.length; i++) {
-                    if (i == array.length - 1) break;
-                    if (array[i] < array[i + 1]) { //вынести в виде аргумента
-                        buffer = array[i + 1];
-                        array[i + 1] = array[i];
-                        array[i] = buffer;
-                        elementReplaced = true;
-                    }
-                }
-                break;
+        elementSwitched = false;
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) break;
+            switch (sortingOrder) {
+                case ASCENDING:
+                    if (comparator.compare(array[i], array[i + 1]) == 1) switchElementRight(array, i);
+                    break;
+                case DESCENDING:
+                    if (comparator.compare(array[i], array[i + 1]) == -1) switchElementRight(array, i);
+                    break;
+            }
         }
-        if (elementReplaced == true) sort(array, sortingOrder, comparator);
+        if (elementSwitched == true) sort(array, sortingOrder, comparator);
         return array;
+    }
+
+    private static void switchElementRight(int[] array, int i) {
+        int buffer = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = buffer;
+        elementSwitched = true;
     }
 }
